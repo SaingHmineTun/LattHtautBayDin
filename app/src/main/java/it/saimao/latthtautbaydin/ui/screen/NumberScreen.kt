d2e84dba4b7874ccdfb6c529bbe3fa7cc9ae8d4d
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2024-02-14 14:29:03 +0630

    Updated: Improve some design!!!

--- app/src/main/java/it/saimao/latthtautbaydin/ui/screen/NumberScreen.kt
+++ app/src/main/java/it/saimao/latthtautbaydin/ui/screen/NumberScreen.kt
@@ -2,7 +2,6 @@ package it.saimao.latthtautbaydin.ui.screen
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -33,7 +32,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -76,13 +74,13 @@ fun ChooseNumberScreen(
         }
         when (numberScreenTab) {
             0 -> NumberScreen(onSelectNumber = onSelectNumber)
-            1 -> RandomScreen(onSelectNumber = onSelectNumber)
+            1 -> SpinScreen(onSelectNumber = onSelectNumber)
         }
     }
 }
 
 @Composable
-fun RandomScreen(onSelectNumber: (Int) -> Unit, modifier: Modifier = Modifier) {
+fun SpinScreen(onSelectNumber: (Int) -> Unit, modifier: Modifier = Modifier) {
 
     val imageResources = arrayOf(
         R.drawable.one,
@@ -187,7 +185,6 @@ fun NumberScreen(onSelectNumber: (Int) -> Unit, modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .border(
                         1.dp, MaterialTheme.colorScheme.primary,
-
                         shape = CutCornerShape(2.dp),
                     )
                     .size(56.dp)
